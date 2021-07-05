9ef8f71e8162dadc65e61768b7d3757c5ff932dc
VictoriaBaral <62349218+VictoriaBaral@users.noreply.github.com>
2021-07-04 21:15:53 -0300

    Agrego cambios al GUI

--- GUI.java
+++ GUI.java
@@ -1,43 +1,46 @@
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JLabel;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-public class GUI implements ActionListener{
-    
-    private int count = 0;
-    private JLabel label;
-    private JFrame frame;
-    private JPanel panel;
-
-    public GUI() {
-
-        frame = new JFrame();
-        JButton button = new JButton("Ingresar");
-        button.addActionListener(this);
-        label = new JLabel("Escuelas");
-
-        panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0,1));
-        panel.add(button);
-        panel.add(label);
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Nuestra GUI");
-        frame.pack();
-        frame.setVisible(true);
-    }
+public class GUI {
 
     public static void main(String[] args){
-        new GUI();
-    }
+        Scanner scanner = new Scanner(System.in);
+        
+        boolean exit = false;
+        int option;
 
-    @Override
-    public void actionPerformed(ActionEvent e){
-        count++;
-        label.setText("Numero de Escuela: " + count);
-    }
+        while(!exit){
+            System.out.println("*****BIENVENIDOS*****");
+            System.out.println("1.Ver lista Alumnos");
+            System.out.println("2.Agregar alumnos");
+            System.out.println("3.Agregar nota alumnos");
+            System.out.println("4.Salir");
 
+            try{
+
+                System.out.println("Introduce una opción: ");
+                option = scanner.nextInt();
+
+                switch(option){
+                    case 1:
+                        System.out.println("Lista Alumnos");
+                        break;
+                    case 2:
+                        System.out.println("Agregar Alumnos");
+                        break;
+                    case 3:
+                        System.out.println("Agregar nota a Alumno");
+                        break;
+                    case 4:
+                        exit=true;
+                        break;
+                    default:
+                        System.out.println("Las opciones son entre 1 al 4");
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Debes elegir una opción");
+                scanner.next();
+        }
+        }
+    }
 }
