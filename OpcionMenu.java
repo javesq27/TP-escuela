85c066733155bec46df837b9e99b61e1916208d1
VictoriaBaral <62349218+VictoriaBaral@users.noreply.github.com>
2021-07-06 19:52:37 -0300

    Agrego interface para el menu, incluyendo sus métodos.

---
+++ OpcionMenu.java
@@ -0,0 +1,10 @@
+
+public interface OpcionMenu {
+    public void opciones();
+    public void AddName();
+    public void AddNotas();
+    public void showstudent();
+    public void showSchools();
+    public void closeapp();
+
+}
