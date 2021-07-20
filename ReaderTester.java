047b9c96c25a5ddbe26df468266119df315e78dd
VictoriaBaral <62349218+VictoriaBaral@users.noreply.github.com>
2021-07-20 20:39:17 -0300

    Agrego Readertester

---
+++ ReaderTester.java
@@ -0,0 +1,16 @@
+public class ReaderTester implements Reader
+{
+    private String selectedOption;
+
+    public String readOption()
+    {
+        return selectedOption;
+    }
+
+    public void setSelectedOption(String selectedOption)
+    {
+        this.selectedOption = selectedOption;
+    }
+
+
+}
